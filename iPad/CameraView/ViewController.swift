150f16190d1a78d7fdb085eeb68b5752596e9369
sonson <yoshida.yuichi@gmail.com>
2017-07-13 01:17:39 +0900

    Booooo, how can I handle CVPixelBufferGetBaseAddress?

--- iPad/CameraView/ViewController.swift
+++ iPad/CameraView/ViewController.swift
@@ -17,12 +17,25 @@ public class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBuf
     let imageView = UIImageView(frame: .zero)
     
     var imageFunc: ((inout [CUnsignedChar], Int, Int, Int) -> Void) = { (pixel: inout [CUnsignedChar], width: Int, height: Int, bytesPerRow: Int) -> Void in
+        
+//        var temp: [CUnsignedChar] = [CUnsignedChar](repeating: 0, count: height * bytesPerRow)
+//
+//        memcpy(&temp, &pixel, height * bytesPerRow)
+        
         for y in 0..<height {
             for x in 0..<width {
-                pixel[4 * x + y * bytesPerRow + 0] = 0
-                //                buffer[4 * x + y * bytesPerRow + 1] = 0
-                //                buffer[4 * x + y * bytesPerRow + 2] = 255
-                //                buffer[4 * x + y * bytesPerRow + 3] = 255
+                
+                let targetx = x //width - 1 - x
+                let targety = height - 1 - y
+                
+                let r = CUnsignedChar(20) //pixel[4 * x + y * bytesPerRow + 0]
+                let g = temp[4 * x + y * bytesPerRow + 1]
+                let b = temp[4 * x + y * bytesPerRow + 2]
+                let a = temp[4 * x + y * bytesPerRow + 3]
+                pixel[4 * targetx + targety * bytesPerRow + 0] = r
+                pixel[4 * targetx + targety * bytesPerRow + 1] = g
+                pixel[4 * targetx + targety * bytesPerRow + 2] = b
+                pixel[4 * targetx + targety * bytesPerRow + 3] = a
             }
         }
     }
@@ -84,18 +97,18 @@ public class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBuf
     
     public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
-        switch connection.videoOrientation {
-        case .portrait:
-            print("portrait")
-        case .portraitUpsideDown:
-            print("portraitUpsideDown")
-        case .landscapeLeft:
-            print("landscapeLeft")
-        case .landscapeRight:
-            print("landscapeRight")
-        }
-        
-        NSLog("[Camera] - capture")
+//        switch connection.videoOrientation {
+//        case .portrait:
+//            print("portrait")
+//        case .portraitUpsideDown:
+//            print("portraitUpsideDown")
+//        case .landscapeLeft:
+//            print("landscapeLeft")
+//        case .landscapeRight:
+//            print("landscapeRight")
+//        }
+//
+//        NSLog("[Camera] - capture")
         let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
         CVPixelBufferLockBaseAddress(pixelBuffer, .readOnly)
 
@@ -106,22 +119,41 @@ public class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBuf
         let height = CVPixelBufferGetHeight(pixelBuffer)
         let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
 
-        let pointer = CVPixelBufferGetBaseAddress(pixelBuffer)
+        let pointer = (CUnsignedChar*)CVPixelBufferGetBaseAddress(pixelBuffer)
         
         var buffer: [CUnsignedChar] = [CUnsignedChar](repeating: 0, count: height * bytesPerRow)
+        
+        
+        
+        for y in 0..<height {
+            for x in 0..<width {
+                
+                let targetx = x //width - 1 - x
+                let targety = height - 1 - y
+                
+//                let r = pointer[4 * x + y * bytesPerRow + 0]
+//                let g = pointer[4 * x + y * bytesPerRow + 1]
+//                let b = pointer[4 * x + y * bytesPerRow + 2]
+//                let a = pointer[4 * x + y * bytesPerRow + 3]
+//                buffer[4 * targetx + targety * bytesPerRow + 0] = r
+//                buffer[4 * targetx + targety * bytesPerRow + 1] = g
+//                buffer[4 * targetx + targety * bytesPerRow + 2] = b
+//                buffer[4 * targetx + targety * bytesPerRow + 3] = a
+            }
+        }
 
-        memcpy(&buffer, pointer, height * bytesPerRow)
+//        memcpy(&buffer, pointer, height * bytesPerRow)
 
         imageFunc(&buffer, width, height, bytesPerRow)
         
         guard let cgImage = creatCGImage(pointer: &buffer, width: pixelBufferWidth, height: pixelBufferHeight, bytesPerPixel: bytesPerHeight) else { return }
 
         let uiImage = UIImage(cgImage: cgImage)
-        CVPixelBufferUnlockBaseAddress(pixelBuffer, .readOnly)
 
         DispatchQueue.main.async {
             self.imageView.image = uiImage
         }
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, .readOnly)
     }
 }
 
